18c00c9eddab8122a7525486534751aaba62b66b
Marcus Rossel <marcusrossel@icloud.com>
2016-03-20 17:36:31 -0600

    Initial Commit

---
+++ LanguageKit/LanguageKit.h
@@ -0,0 +1,19 @@
+//
+//  LanguageKit.h
+//  LanguageKit
+//
+//  Created by Marcus Rossel on 20.03.16.
+//  Copyright © 2016 Marcus Rossel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LanguageKit.
+FOUNDATION_EXPORT double LanguageKitVersionNumber;
+
+//! Project version string for LanguageKit.
+FOUNDATION_EXPORT const unsigned char LanguageKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LanguageKit/PublicHeader.h>
+
+
